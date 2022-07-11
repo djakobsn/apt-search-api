738a90cfb80a327fe71001fd97a036b8af2458db
Daniel Jakobsen <daniel.jakobsen.ext@tribia.com>
2022-07-11 09:37:29 +0200

    Initialize repo for first attempt

---
+++ src/main/java/no/jkbn/apt_search_app/AptSearchAppApplication.java
@@ -0,0 +1,13 @@
+package no.jkbn.apt_search_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptSearchAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptSearchAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/jkbn/apt_search_app/AptSearchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package no.jkbn.apt_search_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptSearchAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
